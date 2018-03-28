45f5730ec181fae47ee2ba8d82d2f4a6f33f8042
html-ninja <36866588+html-ninja@users.noreply.github.com>
2018-03-28 22:35:02 +0500

    правки

--- js/catalog.js
+++ js/catalog.js
@@ -2,11 +2,23 @@ var cartLink = document.querySelectorAll(".buy");
 var cartPopup = document.querySelector(".modal-catalog");
 var cartClose = cartPopup.querySelector(".modal-close-cart");
 
-cartLink.addEventListener("click", function (evt) {
-   evt.preventDefault();
-   cartPopup.classList.add("modal-cart-show");
-  });
-  cartClose.addEventListener("click", function (evt) {
+for (var i = 0; i < cartLink.length; i++) {
+addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.add("modal-cart-show");
+addEventListener("click", function (evt) {
     evt.preventDefault();
     cartPopup.classList.remove("modal-cart-show");
   });
+});
+}
+
+var bookmarkLink = document.querySelectorAll(".bookmark");
+var bookmarkPopup = document.querySelector(".store-nav-bookmarks");
+
+for (var i = 0; i < bookmarkLink.length; i++) {
+addEventListener("click", function (evt) {
+  evt.preventDefault();
+  bookmarkPopup.classList.add("item-added-to-cart");
+});
+}
--- js/pop-up.js
+++ js/pop-up.js
@@ -3,24 +3,24 @@ var popup = document.querySelector(".modal-window");
 var close = popup.querySelector(".modal-close");
 
 link.addEventListener("click", function (evt) {
-   evt.preventDefault();
-   popup.classList.add("modal-window-show");
-  });
-  close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-window-show");
-  });
+  evt.preventDefault();
+  popup.classList.add("modal-window-show");
+});
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-window-show");
+});
 
 
-  var mapLink = document.querySelector(".map-link");
-  var mapPopup = document.querySelector(".map-pop-up");
-  var mapClose = mapPopup.querySelector(".modal-close-map");
+var mapLink = document.querySelector(".map-link");
+var mapPopup = document.querySelector(".map-pop-up");
+var mapClose = mapPopup.querySelector(".modal-close-map");
 
-  mapLink.addEventListener("click", function (evt) {
-     evt.preventDefault();
-     mapPopup.classList.add("modal-map-show");
-    });
-    mapClose.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      mapPopup.classList.remove("modal-map-show");
-    });
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-map-show");
+});
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-map-show");
+});
