36593af5f9a2f3a416c5b2cac012e3fafc9780e7
html-ninja <36866588+html-ninja@users.noreply.github.com>
2018-04-01 02:25:05 +0500

    global changes 1.04

--- js/pop-up.js
+++ js/pop-up.js
@@ -28,83 +28,14 @@ var cartLink = document.querySelectorAll(".buy")
 var cartPopup = document.querySelector(".modal-catalog");
 var cartClose = cartPopup.querySelector(".modal-close-cart");
 
-cartLink[0].addEventListener("click", function (evt) {
-  evt.preventDefault();
-- cartPopup.classList.add("modal-cart-show");
-});
-cartClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.remove("modal-cart-show");
-});
-
-cartLink[1].addEventListener("click", function (evt) {
+for(var i=0; i < cartLink.length; i++) {
+ cartLink[i].addEventListener("click", function (evt) {
   evt.preventDefault();
   cartPopup.classList.add("modal-cart-show");
 });
+}
+
 cartClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   cartPopup.classList.remove("modal-cart-show");
-});
-
-cartLink[2].addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.add("modal-cart-show");
-});
-cartClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.remove("modal-cart-show");
-});
-
-cartLink[3].addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.add("modal-cart-show");
-});
-cartClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.remove("modal-cart-show");
-});
-
-cartLink[4].addEventListener("click", function (evt) {
-      evt.preventDefault();
-      cartPopup.classList.add("modal-cart-show");
-});
-      cartClose.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      cartPopup.classList.remove("modal-cart-show");
-});
-
-cartLink[5].addEventListener("click", function (evt) {
-      evt.preventDefault();
-      cartPopup.classList.add("modal-cart-show");
-});
-      cartClose.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      cartPopup.classList.remove("modal-cart-show");
-});
-
-cartLink[6].addEventListener("click", function (evt) {
-      evt.preventDefault();
-      cartPopup.classList.add("modal-cart-show");
-});
-      cartClose.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      cartPopup.classList.remove("modal-cart-show");
-});
-
-cartLink[7].addEventListener("click", function (evt) {
-      evt.preventDefault();
-      cartPopup.classList.add("modal-cart-show");
-});
-      cartClose.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      cartPopup.classList.remove("modal-cart-show");
-});
-
-cartLink[8].addEventListener("click", function (evt) {
-      evt.preventDefault();
-      cartPopup.classList.add("modal-cart-show");
-});
-      cartClose.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      cartPopup.classList.remove("modal-cart-show");
 });
